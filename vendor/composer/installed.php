99ed20842e804e0ca20b7f4ff105308b3df2a8ee
moserov <profi.michi.7@web.de>
2022-01-12 23:39:22 +0100

    commit 6

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2ffede5ed95830de7fa878bdf2f5b290424b2a31',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2ffede5ed95830de7fa878bdf2f5b290424b2a31',
+            'dev_requirement' => false,
+        ),
+        'facebook/graph-sdk' => array(
+            'pretty_version' => '5.1.4',
+            'version' => '5.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../facebook/graph-sdk',
+            'aliases' => array(),
+            'reference' => '38fd7187a6704d3ab14ded2f3a534ac4ee6f3481',
+            'dev_requirement' => false,
+        ),
+    ),
+);
