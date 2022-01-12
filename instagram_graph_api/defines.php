2ffede5ed95830de7fa878bdf2f5b290424b2a31
moserov <profi.michi.7@web.de>
2022-01-12 23:30:41 +0100

    commit 6

--- instagram_graph_api/defines.php
+++ instagram_graph_api/defines.php
@@ -2,8 +2,8 @@
 	session_start();
 
 	define( 'FACEBOOK_APP_ID', '889461408398528' );
-	define( 'FACEBOOK_APP_SECRET', 'e4f3a20599f38c485702f5a98f53ab9f' );
-	define( 'FACEBOOK_REDIRECT_URI', 'http://localhost/' );
+	define( 'FACEBOOK_APP_SECRET', '' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://example.com/' );
 	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v5.0/' );
 
 	// accessToken
