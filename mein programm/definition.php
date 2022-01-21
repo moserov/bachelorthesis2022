be242cffd35c9f30bf6b784641f7372e4256015b
moserov1 <moserov@smail.uni-koeln.de>
2022-01-21 01:32:43 +0100

    definition file changed

--- mein programm/definition.php
+++ mein programm/definition.php
@@ -1,17 +1,5 @@
 <?php
 	session_start();
 
-	define( 'FACEBOOK_APP_ID', '301944075074469' );
-	define( 'FACEBOOK_APP_SECRET', '5a1bf9f7fae4f071576ac696e31d5d3b' );
-	define( 'FACEBOOK_REDIRECT_URI', 'https://example.com/' );
-	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v12.0/' );
-
-	// accessToken
-	$accessToken = 'EAAESndVWO6UBAM3NIZCon5mqehJRFSs1ePpMm5MAZB8G1XOrNDmQStZAEnQOfuX61h3XJgTelnsZBQTuuIRungqAflvO4nuMCMRmOeONTDwneElgNhsMJNXakcZCH9NLLo2PJDehMJG6rvtop8FH2rVZBiOoSJ6NKxpmQ9mvWOBe7eoRoZAamX8';
-	// page id
-	$pageId = '101145875804586';
-
-	// instagram business account id
-	$instagramAccountId = '17841451644882409';
 	
 ?>
